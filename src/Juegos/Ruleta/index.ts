bcc6004e971564b444b897963238775ec0fffc5c
vaninaL <vani_labrunee@hotmail.com>
2025-05-29 18:36:38 -0300

    trabaje en la clase ruleta

--- src/Juegos/Ruleta/index.ts
+++ src/Juegos/Ruleta/index.ts
@@ -4,9 +4,9 @@ import * as rs from "readline-sync";
 class Ruleta {
     private numeroGanador:number;
     private numeroApostado:number[] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
-    private apuesta:number;
+    private valorApuesta:number;
     private premio:number;
-    private saldo:number = 0 ;
+    private saldo:number = 0 ; // usar saldo de casino
     private color:string[] = ["ROJO","NEGRO"];
     private parImpar:string[] = ["PAR","IMPAR"];
     
